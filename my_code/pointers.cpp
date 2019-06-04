7c614d1ae55f94ee7e0b12a6ff1f9cf74b102817
YianLu <51104015+YianLu@users.noreply.github.com>
2019-06-04 17:07:31 -0400

    Update pointers.cpp

--- my_code/pointers.cpp
+++ my_code/pointers.cpp
@@ -4,63 +4,33 @@
 using namespace std;
 
 void inc_by_value(int n) {
-	++n;
 }
 
 
 void inc_with_pointer(int* iptr) {
-	++(*iptr);
 }
 
 
 void inc_with_reference(int& n) {
-	++n;
 }
 
 
 Thing** create_array_of_things(int n) {
-	Thing** tarray = new Thing* [n];
-	for (int i = 0; i < n; i++) {
-		tarray[i] = new Thing(i);
-	}
-	return tarray;
+	return nullptr;
 }
 
 void print_all_things(Thing** things, int n) {
-	for (int i = 0; i < n; i++) {
-		cout << things[i]->val << " ";
-	}
-	cout << endl;
 }
 
 void double_all_things(Thing** things, int n) {
-	for (int i = 0; i < n; i++) {
-		things[i]->val *=2;
-	}
 }
 
 void delete_all_things(Thing** things, int n) {
-	for (int i = 0; i < n; i++) {
-		delete things[i];
-
-	}
-	delete[]things;
 }
 
 
 void assignTA(Student* s, Student* ta) {
-	s->ta = ta;
 }
 
 void printTAs(vector<Student*>& students) {
-	for (Student* student : students) {
-		if (student->ta == nullptr) {
-			cout << student->name << " has no TA\n";
-		}
-		else {
-			cout << student->name << " has TA " << student->ta->name << endl;
-		}
-		
-	}
-
 }
